6be8caab2fabed357f5b52d02b4069ab487265d6
eyckwu <eyckwu@gmail.com>
2019-07-14 13:43:24 +0800

    桶排序

--- src/test/chapter_0000/BucketSortTest.java
+++ src/test/chapter_0000/BucketSortTest.java
@@ -1,20 +1,20 @@
 package test.chapter_0000;
 
+import chapter_0000.BucketSort;
 import chapter_0000.ISortArray;
-import chapter_0000.InsertionSort;
 import org.junit.Test;
 
 import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
-public class InsertionSortTest {
+public class BucketSortTest {
 
     @Test
     public void sort() {
         int[] sourceArray = new int[]{2, 4, 6, 8, 1, 5, 8, 3, 7, 3 ,8, 0};
-        ISortArray insertionSort = new InsertionSort();
-        int[] result = insertionSort.sort(sourceArray);
+        ISortArray bucketSort = new BucketSort();
+        int[] result = bucketSort .sort(sourceArray);
         System.out.println("result:" + Arrays.toString(result));
     }
 }
