fc306ba64999101bd336b76ae5e69d5024dc7e72
WuGuiXin <EyckWu@gmail.com>
2019-07-20 10:41:20 +0800

    二维数组简介

---
+++ src/数组和字符串/Chapter21.java
@@ -0,0 +1,32 @@
+package 数组和字符串;
+
+/**
+ * @author : EyckWu
+ * @date : 2019/7/20
+ * desc    : 二维数组
+ */
+public class Chapter21 {
+    private static void printArray(int[][] a) {
+        for (int i = 0; i < a.length; ++i) {
+            System.out.println(a[i]);
+        }
+        for (int i = 0; i < a.length; ++i) {
+            for (int j = 0; a[i] != null && j < a[i].length; ++j) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Example I:");
+        int[][] a = new int[2][5];
+        printArray(a);
+        System.out.println("Example II:");
+        int[][] b = new int[2][];
+        printArray(b);
+        System.out.println("Example III:");
+        b[0] = new int[3];
+        b[1] = new int[5];
+        printArray(b);
+    }
+}
